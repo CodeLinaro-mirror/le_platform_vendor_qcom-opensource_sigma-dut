01f7352a0d422d80d3516fda0afb8614dd6008c6
Krupali Dhanvijay <quic_kdhanvij@quicinc.com>
2024-08-30 22:35:58 +0530

    AP: Configure fils discovery max interval
    Currently fils discovery max interval is not configured
    hence SAP is not sending fils discovery within an interval
    of 20 TU + 3 TUs.
    
    Configured the fils discovery max interval as 20 TU for the
    6 GHz band and 11ax mode.
    For non-zero interval, the AP schedules fils discovery frames
    to be transmitted for in-band discovery.
    
    Change-Id: I40a059c1ff7e8e4eda8111a83ce57a54b8613b1d
    CRs-Fixed: 3889335

--- ap.c
+++ ap.c
@@ -8515,6 +8515,9 @@ write_conf:
 	if (dut->bridge)
 		fprintf(f, "bridge=%s\n", dut->bridge);
 
+	if (dut->ap_band_6g && mode == AP_11ax && drv == DRIVER_MAC80211)
+		fprintf(f, "fils_discovery_max_interval=20\n");
+
 	if (dut->ap_is_dual && conf_counter == 1) {
 		if (dut->ap_channel_1)
 			fprintf(f, "channel=%d\n", dut->ap_channel_1);
